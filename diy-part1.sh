57bd7f53e5b6bdce7c998ac4707a5f9880af4466
柏万科技 <529961063@qq.com>
2026-09-11 02:04:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 1 (Add custom feeds)
+# Description: OpenWrt DIY script part 1 (Add custom feeds safely)
 
-# 添加 iStore 官方应用商店与其依赖源
-sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
-sed -i '$a src-git nas https://github.com/linkease/nas-packages;master' feeds.conf.default
-sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci;main' feeds.conf.default
+# 仅在不存在对应源时追加，防止 feeds update 时抛出 Duplicate feed name
+grep -q "src-git istore" feeds.conf.default || echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+grep -q "src-git nas " feeds.conf.default || echo 'src-git nas https://github.com/linkease/nas-packages;master' >> feeds.conf.default
+grep -q "src-git nas_luci" feeds.conf.default || echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci;main' >> feeds.conf.default
